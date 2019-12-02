947e0141b9f00425a409022fe3795c7ec047c447
Shawn <shawnw.mobile@gmail.com>
2019-12-02 01:47:24 -0800

    Fancier Day 2

--- 2019/day02.tcl
+++ 2019/day02.tcl
@@ -3,54 +3,70 @@
 proc run {uinsns} {
     upvar $uinsns insns
     set ip 0
-    while {1} {
-        set insn [lindex $insns $ip]
-        if {$insn == 1} { # Addition
-            set r0 [lindex $insns [lindex $insns $ip+1]]
-            set r1 [lindex $insns [lindex $insns $ip+2]]
-            set r2 [lindex $insns $ip+3]
-            lset insns $r2 [expr {$r0 + $r1}]
-            incr ip 4
-        } elseif {$insn == 2} { # Multiplication
-            set r0 [lindex $insns [lindex $insns $ip+1]]
-            set r1 [lindex $insns [lindex $insns $ip+2]]
-            set r2 [lindex $insns $ip+3]
-            lset insns $r2 [expr {$r0 * $r1}]
-            incr ip 4
-        } elseif {$insn == 99} { # Halt
-            return [lindex $insns 0]
-        } else {
-            puts "Unknown opcode $insn at position $ip"
-            return -1
+    while 1 {
+        switch [lindex $insns $ip] {
+            1 { # Addition
+                set r0 [lindex $insns [lindex $insns $ip+1]]
+                set r1 [lindex $insns [lindex $insns $ip+2]]
+                set r2 [lindex $insns $ip+3]
+                lset insns $r2 [expr {$r0 + $r1}]
+                incr ip 4
+            }
+            2 { # Multiplication
+                set r0 [lindex $insns [lindex $insns $ip+1]]
+                set r1 [lindex $insns [lindex $insns $ip+2]]
+                set r2 [lindex $insns $ip+3]
+                lset insns $r2 [expr {$r0 * $r1}]
+                incr ip 4
+            }
+            99 { # Halt
+                return [lindex $insns 0]
+            }
+            default {
+                puts "Unknown opcode [lindex $insns $ip] at position $ip"
+                return -1
+            }
         }
     }
 }
 
+proc gen_seq {{n 1}} {
+    yield
+    while 1 {
+        yield $n
+        incr n
+    }
+}
+
+proc runtest {input expected} {
+    puts -nonewline "Test [testno]: $input"
+    run input
+    if {$input == $expected} {
+        puts " passed."
+    } else {
+        puts " failed! Got \[$input\], expected \[$expected\]"
+    }
+}
+
 # Test cases
-if {1} {
-    set testprog [list 1 9 10 3 2 3 11 0 99 30 40 50]
-    run testprog
-    puts "Test 1: $testprog"
-    set testprog [list 1 0 0 0 99]
-    run testprog
-    puts "Test 2: $testprog"
-    set testprog [list 2 3 0 3 99]
-    run testprog
-    puts "Test 3: $testprog"
-    set testprog [list 2 4 4 5 99 0]
-    run testprog
-    puts "Test 4: $testprog"
-    set testprog [list 1 1 1 4 99 5 6 0 99]
-    run testprog
-    puts "Test 5: $testprog"
+if 1 {
+    coroutine testno gen_seq
+    runtest {1 9 10 3 2 3 11 0 99 30 40 50} {3500 9 10 70 2 3 11 0 99 30 40 50}
+    runtest {1 0 0 0 99} {2 0 0 0 99}
+    runtest {2 3 0 3 99} {2 3 0 6 99}
+    runtest {2 4 4 5 99 0} {2 4 4 5 99 9801}
+    runtest {1 1 1 4 99 5 6 0 99} {30 1 1 4 2 5 6 0 99}
+    rename testno {}
 }
 
 gets stdin line
 set insns [split $line ","]
+
 set p1_insns $insns
 lset p1_insns 1 12
 lset p1_insns 2 2
-puts "Part1: [run p1_insns]"
+puts "Part 1: [run p1_insns]"
+
 for {set noun 0} {$noun < 100} {incr noun} {
     for {set verb 0} {$verb < 100} {incr verb} {
         set p2_insns $insns
