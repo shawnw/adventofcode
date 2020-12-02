645e824c904b35e8dacfc7e36a394186ddc08cbf
Shawn <shawnw.mobile@gmail.com>
2020-12-02 15:10:59 -0800

    Better day02 tcl version

--- 2020/day02.tcl
+++ 2020/day02.tcl
@@ -23,12 +23,13 @@ proc solve2 {input} {
         if {[regexp $::re $line -> idx1 idx2 char password]} {
             incr idx1 -1
             incr idx2 -1
+            set n [expr {$idx2 - $idx1 - 1}]
             set validator \
-                [string cat \
-                     "(?:^[string repeat . $idx1]$char[string repeat . [expr {$idx2-$idx1-1}]]\[^$char\])" \
-                     "|" \
-                     "(?:^[string repeat . $idx1]\[^$char\][string repeat . [expr {$idx2-$idx1-1}]]$char)"]
-            if {[regexp $validator $password]} { incr valid }
+                [format {(?:^.{%d}%s.{%d}[^%s])
+                         |
+                         (?:^.{%d}[^%s].{%d}%s)} \
+                     $idx1 $char $n $char $idx1 $char $n $char]
+            if {[regexp -expanded $validator $password]} { incr valid }
         }
     }
     puts $valid
